68bdf2e94c8ab0d90e84656ec5c28e41c370247b
Michael Galloy <mgalloy@gmail.com>
2020-03-03 16:35:33 -0700

    Add ability to validate a file

---
+++ bin/ucomp_validate_file.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# This script launches IDL to validate a UCoMP data file.
+
+canonicalpath() {
+  if [ -d $1 ]; then
+    pushd $1 > /dev/null 2>&1
+    echo $PWD
+  elif [ -f $1 ]; then
+    pushd $(dirname $1) > /dev/null 2>&1
+    echo $PWD/$(basename $1)
+  else
+    echo "Invalid path $1"
+  fi
+  popd > /dev/null 2>&1
+}
+
+# find locations relative to this script
+SCRIPT_LOC=$(canonicalpath $0)
+BIN_DIR=$(dirname ${SCRIPT_LOC})
+
+source ${BIN_DIR}/ucomp_include.sh
+
+# reset DATE because it's different in ucomp_include.sh
+if [[ $# -lt 2 ]]; then
+  DATE=$(date +"%Y%m%d" -d "-1 day")
+else
+  DATE=$2
+fi
+
+if [[ $# -lt 3 ]]; then
+  # TODO: error, filename must be specified
+  FILENAME=
+else
+  FILENAME=$3
+fi
+
+if [[ $# -lt 4 ]]; then
+  LEVEL=0
+else
+  LEVEL=$4
+fi
+
+${IDL} -quiet -IDL_QUIET 1 -IDL_STARTUP "" \
+  -IDL_PATH ${UCOMP_PATH} -IDL_DLM_PATH ${UCOMP_DLM_PATH} \
+  -e "ucomp_validate_file, '${DATE}', '${CONFIG}', '${FILENAME}', ${LEVEL}"
+
+exit $?
