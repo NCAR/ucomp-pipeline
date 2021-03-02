3b4817843727f0e387d28b219f4a9d9cf61b2bac
Michael Galloy <mgalloy@gmail.com>
2021-03-02 10:18:26 -0700

    Find BIN_DIR more robustly via cannonical_path

--- bin/ucomp_validate.sh
+++ bin/ucomp_validate.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
+canonicalpath() {
+  if [ -d $1 ]; then
+    pushd $1 > /dev/null 2>&1
+    echo $PWD
+  elif [ -f $1 ]; then
+    pushd $(dirname $1) > /dev/null 2>&1
+    echo $PWD/$(basename $1)
+  else
+    echo "Invalid path $1"
+  fi
+  popd > /dev/null 2>&1
+}
+
 # run on a week, starting from 2 days before the current date
 START_DATE=$(date +"%Y%m%d" -d "-9 days")
 END_DATE=$(date +"%Y%m%d" -d "-2 days")
 
-BIN_DIR=$(dirname $0)
+# find locations relative to this script
+SCRIPT_LOC=$(canonicalpath $0)
+BIN_DIR=$(dirname ${SCRIPT_LOC})
 
 LOG_FILENAME=/tmp/ucomp-$RANDOM.log
 
