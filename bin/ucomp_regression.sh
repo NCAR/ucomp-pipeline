9fcaa452ab0018f2e728ccb69e53463c9e47c1e8
Michael Galloy <mgalloy@gmail.com>
2021-03-26 16:19:04 -0600

    Adding regression tests to nightly run

---
+++ bin/ucomp_regression.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+canonicalpath() {
+  if [ -d $1 ]; then
+    pushd $1 > /dev/null 2>&1
+    echo $PWD
+  elif [ -f $1 ]; then
+    pushd $(dirname $1) > /dev/null 2>&1
+    echo $PWD/$(basename $1)
+  else
+    echo "Invalid path $1"
+  fi
+  popd > /dev/null 2>&1
+}
+
+
+# find locations relative to this script
+SCRIPT_LOC=$(canonicalpath $0)
+BIN_DIR=$(dirname ${SCRIPT_LOC})
+PIPE_DIR=$(dirname ${BIN_DIR})
+
+source ${BIN_DIR}/ucomp_include.sh
+
+DATES=(20200825 20210204)
+LOG_DIR=/hao/twilight/Data/UCoMP/logs.regression
+
+CONFIG=${PIPE_DIR}/config/ucomp.regression.cfg
+STATUS=0
+for day in ${dates[@]}; do
+  ${IDL} -quiet -IDL_QUIET 1 -IDL_STARTUP "" \
+    -IDL_PATH ${UCOMP_PATH} -IDL_DLM_PATH ${UCOMP_DLM_PATH} \
+    -e "ucomp_regression_wrapper, '${day}', '${CONFIG}'" > /dev/null 2>&1
+  if [ $? -ne 0 ]; then STATUS=$(( ${STATUS} | $? )); fi
+  cat ${LOG_DIR}/${day}.ucomp.regress.log
+done
+
+exit ${STATUS}
