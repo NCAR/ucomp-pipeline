c4da4d47121be3b23511213437941fbb76631af9
Michael Galloy <mgalloy@gmail.com>
2021-09-28 21:07:24 -0600

    Work on C distortion

--- src/level1/ucomp_level1.c
+++ src/level1/ucomp_level1.c
@@ -27,7 +27,7 @@ static IDL_VPTR IDL_ucomp_quick_demodulation(int argc, IDL_VPTR *argv) {
   IDL_VPTR dmatrix_vptr = argv[0];
   IDL_VPTR data_vptr    = argv[1];
   int n_dims;
-  int x, n_cols, y, n_rows, p, n_polstates, c, n_cameras, e, n_extensions, d;
+  int x, n_cols, y, n_rows, n_polstates, c, n_cameras, e, n_extensions;
   int p_col, p_row;
   IDL_VPTR result;
   float *result_data, *dmatrix, *data;
@@ -64,19 +64,56 @@ static IDL_VPTR IDL_ucomp_quick_demodulation(int argc, IDL_VPTR *argv) {
   return(result);
 }
 
+
+// data = ucomp_quick_distortion(data, dx0_c, dy0_c, dx1_c, dy1_c)
+//
+// x = dindgen(nx, ny) mod nx
+// y = transpose(dindgen(ny, nx) mod ny)
+//
+// dist_corrected = interpolate(sub_image, $
+//                              x + ucomp_eval_surf(dx_c, dindgen(nx), dindgen(ny)), $
+//                              y + ucomp_eval_surf(dy_c, dindgen(nx), dindgen(ny)), $
+//                              cubic=-0.5, missing=0.0)
 static IDL_VPTR IDL_ucomp_quick_distortion(int argc, IDL_VPTR *argv) {
-  //IDL_VPTR x, IDL_VPTR dx1_c, IDL_VPTR dy1_c, IDL_VPTR dx2_c, IDL_VPTR dy2_c)
-  IDL_VPTR x = argv[0];
-  IDL_VPTR dx1_c = argv[1];
-  IDL_VPTR dy1_c = argv[2];
-  IDL_VPTR dx2_c = argv[3];
-  IDL_VPTR dy2_c = argv[4];
-  int i;
-  //float *result_data = (TYPE *) IDL_MakeTempArray(IDL_TYPE, 2, dims, IDL_ARR_INI_ZERO, &result);
-  for (i = 0; i < 10; i++) {
-    
+  IDL_VPTR data_vptr = argv[0];
+  IDL_VPTR dx0_c_vptr = argv[1];   // 4x4 array of coefficients
+  IDL_VPTR dy0_c_vptr = argv[2];   // 4x4 array of coefficients
+  IDL_VPTR dx1_c_vptr = argv[3];   // 4x4 array of coefficients
+  IDL_VPTR dy1_c_vptr = argv[4];   // 4x4 array of coefficients
+  int n_dims;
+  int x, n_cols, y, n_rows, p, n_polstates, c, n_cameras, e, n_extensions;
+  IDL_VPTR result;
+  float *result_data, *data, *dx0_c_data, *dy0_c_data, *dx1_c_data, *dy1_c_data;
+  float *dx0_surface, *dy0_surface, *dx1_surface, *dy1_surface;
+
+  // TODO: allocate surfaces
+  // TODO: initialize surfaces
+
+  result_data = (float *)IDL_VarMakeTempFromTemplate(data_vptr,
+                                                     IDL_TYP_FLOAT,
+                                                     NULL,
+                                                     &result,
+                                                     1);
+  n_dims = data_vptr->value.arr->n_dim;
+  n_cols = data_vptr->value.arr->dim[0];
+  n_rows = data_vptr->value.arr->dim[1];
+  n_polstates = data_vptr->value.arr->dim[2];
+  n_cameras = data_vptr->value.arr->dim[3];
+  n_extensions = n_dims < 5 ? 1 : data_vptr->value.arr->dim[4];
+
+  for (e = 0; e < n_extensions; e++) {
+    for (c = 0; c < n_cameras; c++) {
+      for (p = 0; p < n_polstates; p++) {
+        for (y = 0; y < n_rows; y++) {
+          for (x = 0; x < n_cols; x++) {
+            // TODO: interpolate
+          }
+        }
+      }
+    }
   }
-  return x;
+
+  return(result);
 }
 
 
