4bf1b0fb8bb3fcf8fdc329a88cefb58cce7515b7
Michael Galloy <mgalloy@gmail.com>
2022-02-14 08:43:00 -0700

    Better executable permissions on bin files installed

---
+++ bin/ucomp_realtime_today.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+canonicalpath() {
+  if [ -d $1 ]; then
+    pushd $1 > /dev/null 2>&1
+    echo $PWD
+  elif [ -f $1 ]; then
+    pushd $(dirname $1) > /dev/null 2>&1
+    echo $PWD/$(basename $1)
+  else
+    echo "Invalid path $1"
+  fi
+  popd > /dev/null 2>&1
+}
+
+
+# find locations relative to this script
+SCRIPT_LOC=$(canonicalpath $0)
+BIN_DIR=$(dirname ${SCRIPT_LOC})
+PIPE_DIR=$(dirname ${BIN_DIR})
+
+CONFIG=${PIPE_DIR}/config/ucomp.${1}.cfg
+
+source ${BIN_DIR}/ucomp_include.sh
+
+TODAY=$(date +"%Y%m%d")
+
+${IDL} -quiet -IDL_QUIET 1 -IDL_STARTUP "" \
+  -IDL_PATH ${UCOMP_PATH} -IDL_DLM_PATH ${UCOMP_DLM_PATH} \
+  -e "ucomp_eod_wrapper, '${TODAY}', '${CONFIG}'"
+
+exit $?
