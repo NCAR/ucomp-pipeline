bf5b2bd9772f1c009e3a96e4226deac7d7798c78
Michael Galloy <mgalloy@gmail.com>
2025-02-07 16:20:13 -0700

    Cleaning up Chianti table generation for #172

--- analysis/ucomp_density_ratios.py
+++ analysis/ucomp_density_ratios.py
@@ -13,12 +13,10 @@ def write_ncdf(filename: str, heights, densities, ratios, info):
 
     rootgrp.chianti_version = info["chianti_version"]
     rootgrp.electron_temperature = info["electron_temperature"]
-    abundances_filename = info["abundances_filename"]
-    rootgrp.abundances_filename = abundances_filename if abundances_filename is not None else "default"
+    abundances_basename = info["abundances_basename"]
+    rootgrp.abundances_basename = abundances_basename if abundances_basename is not None else ""
     n_levels = info["n_levels"]
-    rootgrp.n_levels = float(n_levels) if n_levels is not None else "all"
-    # rootgrp.limbdark = "yes" if info["limbdark"] else "no"
-    # rootgrp.protons = "yes" if info["protons"] else "no"
+    rootgrp.n_levels = float(n_levels) if n_levels is not None else np.nan
     rootgrp.limbdark = np.uint8(info["limbdark"])
     rootgrp.protons = np.uint8(info["protons"])
 
@@ -36,18 +34,23 @@ def write_ncdf(filename: str, heights, densities, ratios, info):
     rootgrp.close()
 
 
-def compute_ratios(limbdark=True, protons=True, n_levels=None,
+def compute_ratios(abundances_basename=None, limbdark=True, protons=True,
+                   n_levels=None,
                    n_heights: int=99, min_height=1.01, max_height=2.0,
                    n_densities: int=120, min_density=6.0, max_density=12.0):
     chianti_dir, chianti_version = pycelp.chianti.getChiantiDir()
-    # abundances_filename = os.path.join(chianti_dir,
-    #     "abundance", "sun_coronal_2021_chianti.abund")
-    abundances_filename = None
+
+    if abundances_basename is not None:
+        abundances_filename = os.path.join(chianti_dir,
+            "abundance", f"{abundances_basename}.abund")
+    else:
+        abundances_filename = None
 
     fe13 = pycelp.Ion("fe_13", nlevels=n_levels, abundFile=abundances_filename)
+
+    # This produces a temperature of 1676832 K, whereas Solarsoft Chianti
+    # routine MAX_TEMP give a temperature of 1778279 K.
     electron_temperature = fe13.get_maxtemp()
-    # vs. Solarsoft max_temp('fe xiii')
-    # electron_temperature = 1778279.410038923
 
     densities   = 10.0**np.linspace(min_density, max_density, n_densities)
     heights     = np.linspace(min_height, max_height, n_heights)
@@ -55,10 +58,7 @@ def compute_ratios(limbdark=True, protons=True, n_levels=None,
     n_lines     = 2
     intensities = np.zeros((n_lines, n_heights, n_densities))
 
-    thetaBlocal = 0.0
-
-# black body
-# radtemp=5778.
+    thetaB_local = 0.0
 
     for height_index, height in enumerate(heights):
       print(f"{height_index+ 1} / {n_heights}: {height:0.2f} R_sun")
@@ -66,8 +66,8 @@ def compute_ratios(limbdark=True, protons=True, n_levels=None,
         fe13.calc_rho_sym(density,
                           electron_temperature,
                           height - 1.0,
-                          thetaBlocal,
-                          include_limbdark=limbdark,  # maybe try with off?
+                          thetaB_local,
+                          include_limbdark=limbdark,
                           include_protons=protons)
         line0 = fe13.get_emissionLine(10747.0)
         line1 = fe13.get_emissionLine(10789.0)
@@ -76,9 +76,10 @@ def compute_ratios(limbdark=True, protons=True, n_levels=None,
 
     ratios = intensities[0, :, :] / intensities[1, :, :]
 
-    info = {"chianti_version": chianti_version,
+    info = {
+        "chianti_version": chianti_version,
         "electron_temperature": electron_temperature,
-        "abundances_filename" : abundances_filename,
+        "abundances_basename" : f"{abundances_basename}.abund",
         "n_levels": n_levels,
         "limbdark": limbdark,
         "protons": protons}
@@ -89,23 +90,25 @@ def compute_ratios(limbdark=True, protons=True, n_levels=None,
 if __name__ == "__main__":
     description = "UCoMP density ratio calculator"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("--flag", "-f", default=None, type=str,
-        help="flag to add to output filename")
+    parser.add_argument("--name", "-n", default=None, type=str,
+        help="name to add to output filename")
     parser.add_argument("--output", "-o", default=None, type=str,
         help="output filename")
+    parser.add_argument("--abundances-basename", "-a", default=None, type=str,
+        help="abundances basename, i.e., 'sun_coronal_2021_chianti'")
     parser.add_argument("--no-limbdark", default=False, action="store_true",
         help="set to not include limb darkening")
     parser.add_argument("--no-protons", default=False, action="store_true",
         help="set to not include protons")
-    parser.add_argument("--nlevels", default=None, type=int,
+    parser.add_argument("--n-levels", default=None, type=int,
         help="number of levels (default: all)")
-    parser.add_argument("--nheights", default=99, type=int,
+    parser.add_argument("--n-heights", default=99, type=int,
         help="number of heights (default: 99)")
     parser.add_argument("--min-height", default=1.01, type=float,
         help="min height (default: 1.01)")
     parser.add_argument("--max-height", default=2.0, type=float,
         help="max height (default: 2.0)")
-    parser.add_argument("--ndensities", default=120, type=int,
+    parser.add_argument("--n-densities", default=120, type=int,
         help="number of densities (default: 120)")
     parser.add_argument("--min-density", default=6.0, type=float,
         help="log of min density (default: 6.0)")
@@ -113,15 +116,16 @@ if __name__ == "__main__":
         help="log of max density (default: 12.0)")
     args = parser.parse_args()
 
-    limbdark    = not args.no_limbdark
-    protons     = not args.no_protons
-    n_levels    = args.nlevels
-    n_heights   = args.nheights
-    min_height  = args.min_height
-    max_height  = args.max_height
-    n_densities = args.ndensities
-    min_density = args.min_density
-    max_density = args.max_density
+    abundances_basename = args.abundances_basename
+    limbdark            = not args.no_limbdark
+    protons             = not args.no_protons
+    n_levels            = args.n_levels
+    n_heights           = args.n_heights
+    min_height          = args.min_height
+    max_height          = args.max_height
+    n_densities         = args.n_densities
+    min_density         = args.min_density
+    max_density         = args.max_density
 
     try:
         import pycelp
@@ -133,22 +137,25 @@ if __name__ == "__main__":
     if args.output is not None:
         basename = args.output
     else:
-        flag = f"{args.flag}_" if args.flag is not None else ""
-        basename = f"chianti_v{chianti_version}_pycelp_fe13_h{n_heights}_d{n_densities}_{flag}ratio.nc"
+        name = f"{args.name}_" if args.name is not None else ""
+        basename = f"chianti_v{chianti_version}_pycelp_fe13_h{n_heights}_d{n_densities}_{name}ratio.nc"
 
-    print(f"limb darkening : {'YES' if limbdark else 'NO'}")
-    print(f"protons        : {'YES' if protons else 'NO'}")
-    print(f"# of levels    : {n_levels}")
-    print(f"# of heights   : {n_heights}")
-    print(f"height range   : {min_height:0.3f} - {max_height:0.3f} R_sun")
-    print(f"# of densities : {n_densities}")
-    print(f"density range  : 10**{min_density:0.2f} - 10**{max_density:0.2f}")
+    print(f"abundances basename : {abundances_basename}")
+    print(f"limb darkening      : {'YES' if limbdark else 'NO'}")
+    print(f"protons             : {'YES' if protons else 'NO'}")
+    print(f"# of levels         : {n_levels}")
+    print(f"# of heights        : {n_heights}")
+    print(f"height range        : {min_height:0.3f} - {max_height:0.3f} R_sun")
+    print(f"# of densities      : {n_densities}")
+    print(f"density range       : 10**{min_density:0.2f} - 10**{max_density:0.2f}")
 
     t0 = datetime.datetime.now()
 
-    heights, densities, ratios, info = compute_ratios(limbdark=limbdark,
-        protons=protons, n_levels=n_levels, n_heights=n_heights,
-        min_height=min_height, max_height=max_height, n_densities=n_densities,
+    heights, densities, ratios, info = compute_ratios(
+        abundances_basename=abundances_basename,
+        limbdark=limbdark, protons=protons, n_levels=n_levels,
+        n_heights=n_heights, min_height=min_height, max_height=max_height,
+        n_densities=n_densities,
         min_density=min_density, max_density=max_density)
 
     t1 = datetime.datetime.now()
